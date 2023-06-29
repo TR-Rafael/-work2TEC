f348812d0a2886d4a7009f6f360cd29f7f49978e
Rafael-TR <silva_rafael@id.uff.br>
2023-06-28 22:19:00 -0300

    feat: creation of function amount_of_death_by_age_graph_generator and attempt to create other graphs.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import csv
+import re
+import random
+import numpy as np
 
 def read_csv(pathName):
     payload = []
@@ -19,29 +22,60 @@ def read_csv(pathName):
 
     return payload
 
-def bar_graph_generator(payload):
-    # ['date', 'state', 'gender', 'age', 'color', 'cause', 'total'] <- column_names
-    #  TODO Parei aqui, tava testando algumas coisas da plot lib. Por isto esta esse caos
-    #   Quando retornar irei tentar fazer um grafico que mostre ao longo dos meses quantos homens e mulheres morreram kkk
-    Eixo_x1 = [0.25, 1.25, 2.25, 3.25, 4.25]
-    Eixo_y1 = list(filter(lambda x: x['gender'] == 'M', payload))[0:5]
-    plt.bar(Eixo_x1, Eixo_y1, label="Man", width=.5)
-    #
-    Eixo_x2 = [.75, 1.75, 2.75, 3.75, 4.75]
-    Eixo_y2 = list(filter(lambda x: x.gender == 'F', payload))[0:5]
-    plt.bar(Eixo_x2, Eixo_y2, label="Woman", color='r', width=.5)
+def amount_of_death_by_age_graph_generator(payload):
+    mortes = list(map(lambda x: '0 - 9' if x == '< 9' else x, payload))
+    mortes = list(map(lambda x: '100 - 110' if x == '> 100' else x, mortes))
+    mortes = list(map(lambda x: convert_range_to_random(x), mortes))
+    plt.xlabel("Idades")
+    plt.ylabel("Total de mortes")
+    plt.title("Quantidade de mortes por faixa etaria")
+    plt.hist(mortes, 30, rwidth=0.9)
+    plt.show()
 
-    plt.legend()
-    plt.xlabel("Dias")
-    plt.ylabel("Distância(km)")
-    plt.title("Informação")
+
+def convert_range_to_random(range_string):
+    range_values = re.findall(r'\d+', range_string)
+    if len(range_values) == 2:
+        start = int(range_values[0])
+        end = int(range_values[1])
+        if not any(np.isnan([start, end])):
+            return random.sample(range(start, end + 1), 1)[0]
+    return np.nan
+
+
+def graph2(payload):
+    #  Não ta funfando... Foi uma tentativa de adaptar o gráfico de Idade das mortes de acordo com as etinias
+    #  Com Boxplot, eu tentei ler essa DOC, mas não ajudou muito https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html
+    #  Acho que não da pra fazer esse tipo de gráfico na matplot, mas temos que investigar mais...
+    mortes = list(filter(lambda x: x['color'] != 'Ignored', payload))
+    plt.figure()
+    plt.boxplot(list(map(lambda x: x['age'], mortes)))
+    plt.xlabel("Etinia")
+    plt.ylabel("Idade da Morte")
+    plt.show()
+
+def graph3(payload):
+    mortes = payload
+    # Daonde você tirou esses estados bicho? Preciso dessa info para saber como tratar esse código...
+    #  Isso aqui foi uma tentativa do gráfico de Barras e colunas
+    plt.bar(list(map(lambda x: x['state'], mortes)), height=list(map(lambda x: x['Estados'], mortes)), color='blue')
+    plt.xlabel('state')
+    plt.ylabel('Estados')
     plt.show()
 
 
 if __name__ == "__main__":
+    #  Eu deixei os nomes dos gráficos que não consegui finalizar genéricos mesmo e quando
+    # Conseguirmos fazer, a gente ajeita.
     path_of_database = 'dataBase/death_cause_brazil.csv'
+    data = read_csv(path_of_database)
+    #  Esse abaixo estata funcionando
+    # amount_of_death_by_age_graph_generator(list(map(lambda x: x['age'], data)))
 
-    # for value in lerCSV(path_of_database):
-    #     print(value)
-    bar_graph_generator(read_csv(path_of_database))
-    print('Executou')
+    # Esse a explicação sobre onde parei está dentro da função
+    # graph2(data)
+
+    # Esse a explicação sobre onde parei está dentro da função
+    # graph3(data)
+
+    #O gráfico de linhas eu acho que sei como fazer, vou tentar amanhã.
